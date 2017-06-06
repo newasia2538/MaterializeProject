3ddc62d24860c9d670baee78cc0f83b808c104fc
Athiporn Chantachai <mr.kyoya@gmail.com>
2017-06-06 18:06:16 +0700

    delete css file re-arrange code

--- materialize/js/customjs.js
+++ materialize/js/customjs.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('.button-collapse').sideNav({
         menuWidth: 300, // Default is 300
         edge: 'left', // Choose the horizontal origin
-        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
         draggable: true // Choose whether you can drag to open on touch screens
     });
     $(".dropdown-button").dropdown({
@@ -11,6 +11,7 @@ $(document).ready(function() {
     $('.chips').material_chip();
     $('.collapsible').collapsible();
 });
+
 
 $('.modal').modal({
       dismissible: true, // Modal can be dismissed by clicking outside of the modal
